d7cffcf1a2e5e4de4b92567fbd432fac77798907
Rachel Freeland <rachel.freeland1@gmail.com>
2022-04-27 01:26:57 -0500

    Renaming files

---
+++ pythonic_garage_band/band.py
@@ -0,0 +1,16 @@
+class Band:
+    def __init__(self, name):
+        self.name = name
+        return self.name
+
+class Musician(Band):
+    pass
+
+class Guitarist(Musician):
+    pass
+
+class Bassist(Musician):
+    pass
+
+class Drummer(Musician):
+    pass
